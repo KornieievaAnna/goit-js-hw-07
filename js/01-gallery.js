519eb7dd361c8fd69e55cd6e473e99a3de083db5
Anna Kornieieva <109692085+KornieievaAnna@users.noreply.github.com>
2022-12-14 21:58:13 +0200

    task 1 dane

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const list = document.querySelector(".gallery");
+
+for (const { preview, original, description } of galleryItems) { 
+    
+ const string = `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    list.insertAdjacentHTML("afterbegin", string);
+}
+
+list.addEventListener("click", onclick);
+
+function onclick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+    const instance = basicLightbox.create(`<img src="${evt.target.dataset.source}" width="800" height="600">`);
+    instance.show();
+
+    console.log(evt.target.dataset.source);
+  
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector(".gallery");
+
+for (const { preview, original, description } of galleryItems) {
+  const string = `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}n" />
+</a>`;
+  list.insertAdjacentHTML("afterbegin", string);
+}
+function onclick(evt) {
+    evt.preventDefault();
+    if (evt.target.nodeName !== "IMG") {
+        return;
+    }
+
+}
+
